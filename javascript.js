47732b165f723ff338537e25921f10c78b7e0583
Ricky Flynn <adiosk8a7x@gmail.com>
2022-08-19 18:20:18 -0800

    Add the psuedocode comments for guidance

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Rock Paper Scissors
+
+// choices - rock, paper, scissors
+
+// getComputerChoice() - return random choice between choices 
+
+// playerSelection() - prompt player for their choice
+// convert toLowerCase() for validation
+
+// computerSelection = getComputerChoice()
+
+// playerScore starts at 0
+
+// computerScore starts at 0
+
+// playRound() - compare computerSelection with playerSelection to determine
+//  winner, if draw go again. Increment winners score by 1. Announce win or 
+//  lose round.
+
+// game() - play 5 rounds, highest score at end is winner, announce winner.
+
